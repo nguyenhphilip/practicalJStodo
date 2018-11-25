089edafda1c733fc342a22ab8966b9f541823964
Phil Nguyen <phil@scottsMBookgrey.localdomain>
2018-11-25 12:20:29 -0500

    Add version 3 - objects

--- JS/todo1.js
+++ JS/todo1.js
@@ -1,1 +1,23 @@
-window.alert("Hello");
+// Version 3 //
+var todo = {
+  todoList: [],
+  displayTodo: function(){
+    console.log("My todos:",this.todoList);
+  },
+  addTodo: function(item){
+    this.todoList.push(item);
+    this.displayTodo();
+  },
+  changeTodo: function(position, item){
+    if (position > this.todoList.length | position < 0) {
+      console.log("The list isn't that big yet. Try starting with ", this.todoList.length);
+    } else {
+    this.todoList[position] = item;
+    this.displayTodo();
+    }
+  },
+  deleteTodo: function(position, item){
+    this.todoList.splice(position, 1);
+    this.displayTodo();
+  },
+}
